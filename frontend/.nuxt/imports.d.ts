bcbb1ceee3c2ccad0118201e7027e89ee3134e9f
ocsettph <settapong.p@ubu.ac.th>
2026-02-02 10:34:51 +0700

    aigateway.ubu V.5.7.1

--- frontend/.nuxt/imports.d.ts
+++ frontend/.nuxt/imports.d.ts
@@ -31,3 +31,4 @@ export { setInterval } from '#app/compat/interval';
 export { definePageMeta } from '../node_modules/nuxt/dist/pages/runtime/composables';
 export { defineLazyHydrationComponent } from '#app/composables/lazy-hydration';
 export { useApiPath } from '../composables/useApiPath';
+export { useNuxtDevTools } from '../node_modules/@nuxt/devtools/dist/runtime/use-nuxt-devtools';
